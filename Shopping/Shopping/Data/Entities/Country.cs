1b9c1446c0a752d8227a1ac62ea4cf255cba00d1
Diego Alonso Lozano Marroquin <diego_lm91@hotmail.com>
2024-10-05 19:30:06 -0500

    Creacion de proyecto y base de datos

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations; //para usar required y maxlength
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name="País")]
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener maximo {1} caracteres")] //para que el pais tenga maximo 50 caracteres
+        [Required(ErrorMessage ="El campo {0} es obligatorio")] //no va a dejar meter un pais sin nombre, el 0 se reemplaza or el valor de Name
+        public string Name { get; set; }
+    }
+}
